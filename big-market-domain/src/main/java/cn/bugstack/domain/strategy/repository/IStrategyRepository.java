fc9cbed6d21785feb86d9332d4d4b7686f29715f
12480 <1248001568@QQ.COM>
2025-12-04 15:19:39 +0800

    初始化

---
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/repository/IStrategyRepository.java
@@ -0,0 +1,32 @@
+package cn.bugstack.domain.strategy.repository;
+
+import cn.bugstack.domain.strategy.model.entity.StrategyAwardEntity;
+import cn.bugstack.domain.strategy.model.entity.StrategyEntity;
+import cn.bugstack.domain.strategy.model.entity.StrategyRuleEntity;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 策略服务仓储接口
+ * @create 2023-12-23 09:33
+ */
+public interface IStrategyRepository {
+
+    List<StrategyAwardEntity> queryStrategyAwardList(Long strategyId);
+
+    void storeStrategyAwardSearchRateTable(String key, Integer rateRange, Map<Integer, Integer> strategyAwardSearchRateTable);
+
+    Integer getStrategyAwardAssemble(String key, Integer rateKey);
+
+    int getRateRange(Long strategyId);
+
+    int getRateRange(String key);
+
+    StrategyEntity queryStrategyEntityByStrategyId(Long strategyId);
+
+    StrategyRuleEntity queryStrategyRule(Long strategyId, String ruleModel);
+
+}
